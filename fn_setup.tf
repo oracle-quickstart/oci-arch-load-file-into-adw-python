404e9a1e3db8f03aa2ca4e5981c80ac1f6ce4c6e
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-07-30 13:02:03 +0200

    Updated IAM policies

--- fn_setup.tf
+++ fn_setup.tf
@@ -2,15 +2,15 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 resource "null_resource" "Login2OCIR" {
-  depends_on = [oci_functions_application.LoadFileIntoAdwFnApp, 
-                oci_database_autonomous_database.ADWdatabase,
-                oci_objectstorage_bucket.input-bucket,
-                oci_objectstorage_bucket.processed-bucket,
-                null_resource.soda_update,
-                oci_identity_policy.FunctionsServiceReposAccessPolicy,
-                oci_identity_policy.FunctionsServiceNetworkAccessPolicy,
-                oci_identity_dynamic_group.FunctionsServiceDynamicGroup,
-                oci_identity_policy.FunctionsServiceDynamicGroupPolicy]
+  depends_on = [oci_functions_application.LoadFileIntoAdwFnApp,
+    oci_database_autonomous_database.ADWdatabase,
+    oci_objectstorage_bucket.input-bucket,
+    oci_objectstorage_bucket.processed-bucket,
+    null_resource.soda_update,
+    #                oci_identity_policy.FunctionsServiceReposAccessPolicy,
+    #                oci_identity_policy.FunctionsServiceNetworkAccessPolicy,
+    oci_identity_dynamic_group.FunctionsServiceDynamicGroup,
+  oci_identity_policy.FunctionsServiceDynamicGroupPolicy]
 
   provisioner "local-exec" {
     command = "echo '${var.ocir_user_password}' |  docker login ${local.ocir_docker_repository} --username ${local.ocir_namespace}/${var.ocir_user_name} --password-stdin"
@@ -21,22 +21,22 @@ resource "null_resource" "LoadFileIntoAdwFnPush2OCIR" {
   depends_on = [null_resource.Login2OCIR, oci_functions_application.LoadFileIntoAdwFnApp, oci_database_autonomous_database.ADWdatabase]
 
   provisioner "local-exec" {
-    command = "image=$(docker images | grep loadfileintoadw | awk -F ' ' '{print $3}') ; docker rmi -f $image &> /dev/null ; echo $image"
+    command     = "image=$(docker images | grep loadfileintoadw | awk -F ' ' '{print $3}') ; docker rmi -f $image &> /dev/null ; echo $image"
     working_dir = "functions/LoadFileIntoAdw"
   }
 
   provisioner "local-exec" {
-    command = "fn build --verbose"
+    command     = "fn build --verbose"
     working_dir = "functions/LoadFileIntoAdw"
   }
 
   provisioner "local-exec" {
-    command = "image=$(docker images | grep loadfileintoadw | awk -F ' ' '{print $3}') ; docker tag $image ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/loadfileintoadw:0.0.1"
+    command     = "image=$(docker images | grep loadfileintoadw | awk -F ' ' '{print $3}') ; docker tag $image ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/loadfileintoadw:0.0.1"
     working_dir = "functions/LoadFileIntoAdw"
   }
 
   provisioner "local-exec" {
-    command = "docker push ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/loadfileintoadw:0.0.1"
+    command     = "docker push ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/loadfileintoadw:0.0.1"
     working_dir = "functions/LoadFileIntoAdw"
   }
 
--- policies.tf
+++ policies.tf
@@ -3,78 +3,93 @@
 
 # Functions Policies
 
-resource "oci_identity_policy" "FunctionsServiceReposAccessPolicy" {
-  provider = oci.homeregion
-  name = "FunctionsServiceReposAccessPolicy-${random_id.tag.hex}"
-  description = "FunctionsServiceReposAccessPolicy-${random_id.tag.hex}"
-  compartment_id = var.tenancy_ocid
-  statements = ["Allow service FaaS to read repos in tenancy"]
-  provisioner "local-exec" {
-       command = "sleep 5"
-  }
-}
+#resource "oci_identity_policy" "FunctionsServiceReposAccessPolicy" {
+#  provider = oci.homeregion
+#  name = "FunctionsServiceReposAccessPolicy-${random_id.tag.hex}"
+#  description = "FunctionsServiceReposAccessPolicy-${random_id.tag.hex}"
+#  compartment_id = var.tenancy_ocid
+#  statements = ["Allow service FaaS to read repos in tenancy"]
+#  provisioner "local-exec" {
+#       command = "sleep 5"
+#  }
+#}
 
 resource "oci_identity_policy" "FunctionsDevelopersManageAccessPolicy" {
   provider = oci.homeregion
-  depends_on = [oci_identity_policy.FunctionsServiceReposAccessPolicy]
-  name = "FunctionsDevelopersManageAccessPolicy-${random_id.tag.hex}"
-  description = "FunctionsDevelopersManageAccessPolicy-${random_id.tag.hex}"
+  #  depends_on     = [oci_identity_policy.FunctionsServiceReposAccessPolicy]
+  name           = "FunctionsDevelopersManageAccessPolicy-${random_id.tag.hex}"
+  description    = "FunctionsDevelopersManageAccessPolicy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
   statements = ["Allow group Administrators to manage functions-family in compartment id ${var.compartment_ocid}",
-                "Allow group Administrators to read metrics in compartment id ${var.compartment_ocid}"]
+  "Allow group Administrators to read metrics in compartment id ${var.compartment_ocid}"]
   provisioner "local-exec" {
-       command = "sleep 5"
+    command = "sleep 5"
   }
 }
 
 resource "oci_identity_policy" "FunctionsDevelopersManageNetworkAccessPolicy" {
-  provider = oci.homeregion
-  depends_on = [oci_identity_policy.FunctionsDevelopersManageAccessPolicy]
-  name = "FunctionsDevelopersManageNetworkAccessPolicy-${random_id.tag.hex}"
-  description = "FunctionsDevelopersManageNetworkAccessPolicy-${random_id.tag.hex}"
+  provider       = oci.homeregion
+  depends_on     = [oci_identity_policy.FunctionsDevelopersManageAccessPolicy]
+  name           = "FunctionsDevelopersManageNetworkAccessPolicy-${random_id.tag.hex}"
+  description    = "FunctionsDevelopersManageNetworkAccessPolicy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
-  statements = ["Allow group Administrators to use virtual-network-family in compartment id ${var.compartment_ocid}"]
-  
+  statements     = ["Allow group Administrators to use virtual-network-family in compartment id ${var.compartment_ocid}"]
+
   provisioner "local-exec" {
-       command = "sleep 5"
+    command = "sleep 5"
   }
 }
 
-resource "oci_identity_policy" "FunctionsServiceNetworkAccessPolicy" {
-  provider = oci.homeregion
-  depends_on = [oci_identity_policy.FunctionsDevelopersManageNetworkAccessPolicy]
-  name = "FunctionsServiceNetworkAccessPolicy-${random_id.tag.hex}"
-  description = "FunctionsServiceNetworkAccessPolicy-${random_id.tag.hex}"
+#resource "oci_identity_policy" "FunctionsServiceNetworkAccessPolicy" {
+#  provider = oci.homeregion
+#  depends_on = [oci_identity_policy.FunctionsDevelopersManageNetworkAccessPolicy]
+#  name = "FunctionsServiceNetworkAccessPolicy-${random_id.tag.hex}"
+#  description = "FunctionsServiceNetworkAccessPolicy-${random_id.tag.hex}"
+#  compartment_id = var.tenancy_ocid
+#  statements = ["Allow service FaaS to use virtual-network-family in compartment id ${var.compartment_ocid}"]
+#  
+#  provisioner "local-exec" {
+#       command = "sleep 5"
+#  }
+#}
+
+resource "oci_identity_policy" "FunctionsServiceObjectStorageManageAccessPolicy" {
+  provider       = oci.homeregion
+  depends_on     = [oci_identity_policy.FunctionsDevelopersManageNetworkAccessPolicy]
+  name           = "FunctionsServiceObjectStorageManageAccessPolicy-${random_id.tag.hex}"
+  description    = "FunctionsServiceObjectStorageManageAccessPolicy-${random_id.tag.hex}"
   compartment_id = var.tenancy_ocid
-  statements = ["Allow service FaaS to use virtual-network-family in compartment id ${var.compartment_ocid}"]
-  
+  statements     = ["Allow service objectstorage-${var.region} to manage object-family in tenancy"]
+
   provisioner "local-exec" {
-       command = "sleep 5"
+    command = "sleep 5"
   }
 }
+
+
 
 resource "oci_identity_dynamic_group" "FunctionsServiceDynamicGroup" {
   provider = oci.homeregion
-  depends_on = [oci_identity_policy.FunctionsServiceNetworkAccessPolicy]
-  name = "FunctionsServiceDynamicGroup-${random_id.tag.hex}"
-  description = "FunctionsServiceDynamicGroup-${random_id.tag.hex}"
+  #  depends_on     = [oci_identity_policy.FunctionsServiceNetworkAccessPolicy]
+  name           = "FunctionsServiceDynamicGroup-${random_id.tag.hex}"
+  description    = "FunctionsServiceDynamicGroup-${random_id.tag.hex}"
   compartment_id = var.tenancy_ocid
-  matching_rule = "ALL {resource.type = 'fnfunc', resource.compartment.id = '${var.compartment_ocid}'}"
-  
+  matching_rule  = "ALL {resource.type = 'fnfunc', resource.compartment.id = '${var.compartment_ocid}'}"
+
   provisioner "local-exec" {
-       command = "sleep 5"
+    command = "sleep 5"
   }
 }
 
 resource "oci_identity_policy" "FunctionsServiceDynamicGroupPolicy" {
-  provider = oci.homeregion
-  depends_on = [oci_identity_dynamic_group.FunctionsServiceDynamicGroup]
-  name = "FunctionsServiceDynamicGroupPolicy-${random_id.tag.hex}"
-  description = "FunctionsServiceDynamicGroupPolicy-${random_id.tag.hex}"
+  provider       = oci.homeregion
+  depends_on     = [oci_identity_dynamic_group.FunctionsServiceDynamicGroup]
+  name           = "FunctionsServiceDynamicGroupPolicy-${random_id.tag.hex}"
+  description    = "FunctionsServiceDynamicGroupPolicy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
-  statements = ["allow dynamic-group ${oci_identity_dynamic_group.FunctionsServiceDynamicGroup.name} to manage all-resources in compartment id ${var.compartment_ocid}"]
-  
+  statements     = ["allow dynamic-group ${oci_identity_dynamic_group.FunctionsServiceDynamicGroup.name} to manage all-resources in compartment id ${var.compartment_ocid}"]
+
   provisioner "local-exec" {
-       command = "sleep 5"
+    command = "sleep 5"
   }
 }
